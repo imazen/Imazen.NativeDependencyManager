757b1877fa446ce2e3e84c89c622f6a265e26d80
Nathanael Jones <nathanael.jones@gmail.com>
2014-11-29 14:19:43 -0500

    Add FileHashProvider draft

---
+++ src/Imazen.NativeDependencyManager/FileHashProvider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imazen.NativeDependencyManager
+{
+    class FileHashProvider
+    {
+        public FileHashProvider(Func<HashAlgorithm> algorithmPRovider)
+        {
+            AlgorithmProvider = algorithmPRovider;
+        }
+        public FileHashProvider() : 
+            this(() => new SHA1CryptoServiceProvider()) { }
+        public Func<HashAlgorithm> AlgorithmProvider { get; private set; }
+
+        public Task<byte[]> HashStreamAsync(Stream s)
+        {
+            //Todo, replace this with actual async instead of just scheduling a thread for it.
+            return Task.Run<byte[]>(() => HashStream(s));
+        }
+        public byte[] HashStream(Stream s)
+        {
+            using (var alg = AlgorithmProvider()){
+                return alg.ComputeHash(s);
+            }
+        }
+    }
+}
