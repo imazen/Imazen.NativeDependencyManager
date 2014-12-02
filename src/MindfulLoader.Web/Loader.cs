2ebc7a27edd61820e0a7687e9a73b4a77e40ecba
Nathanael Jones <nathanael.jones@gmail.com>
2014-12-02 16:46:47 -0500

    Add MindfulLoader.Web (ASP.NET-specific loader)

---
+++ src/MindfulLoader.Web/Loader.cs
@@ -0,0 +1,38 @@
+﻿using Imazen.NativeDependencyManager;
+using Imazen.NativeDependencyManager.BinaryParsers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MindfulLoader.Web
+{
+    public class Loader
+    {
+        private static IEnumerable<string> GetDylibsInFolder(string dir)
+        {
+            var validExtensions = new string[] { null, "", "dll", "so", "dylib" };
+
+            return Directory.GetFiles(dir)
+                .Where(file => !validExtensions.Contains(Path.GetExtension(file), StringComparer.OrdinalIgnoreCase));
+
+        }
+        private async static Task<Tuple<string,IBinaryInfo>[]> ParseBinaryInfo(IEnumerable<string> files){
+            var tasks = files.Select(path => new BinaryParser().ReadBinaryInfo(path));
+            await Task.WhenAll(tasks);
+            return tasks.Select(t => t.Result).Zip(files, (info, path) => new Tuple<string, IBinaryInfo>(path, info)).ToArray();
+        }
+        public static void PreApplicationStart()
+        {
+            var files = GetDylibsInFolder(HttpRuntime.BinDirectory);
+
+            var parsedFiles = ParseBinaryInfo(files).Result;
+
+            var incompatible = parsedFiles.Where(t => !new CompatChecker().IsCompatible(t.Item2));
+
+        }
+    }
+}
