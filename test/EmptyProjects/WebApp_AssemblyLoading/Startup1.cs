ea1f279623204149adf0a1401fab5613eb525d72
Nathanael Jones <nathanael.jones@gmail.com>
2014-12-02 14:20:53 -0500

    Add test/EmptyProjects/WebApp_AssemblyLoading to test ideas

---
+++ test/EmptyProjects/WebApp_AssemblyLoading/Startup1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace WebApp_AssemblyLoading
+{
+
+    public class PreStartup{
+        public static void PreApplicationStartCode(){
+            AppDomain.CurrentDomain.AssemblyLoad += CurrentDomain_AssemblyLoad;
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+        }
+
+        static System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        static void CurrentDomain_AssemblyLoad(object sender, AssemblyLoadEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
